6449074145cd573e3ba7c3beb011d6c4a8ec2c08
Michael Hester <mhester7@uncc.edu>
2014-10-29 20:53:10 -0300

    hw 2 part 2 (maybe 3) complete_Different way - with case statements-RESTful_too?

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,7 +9,8 @@ class MoviesController < ApplicationController
   end
 
   def index
-	 		
+	
+	sort_by = session[:sort_by] || params[:sort_by]	
 	case		
 		when params[:ratings] == nil && params[:title] == nil && params[:sort_by] == nil
 			@checked = {"G"=>"1", "PG"=>"1", "PG-13"=>"1", "R"=>"1"}
@@ -24,22 +25,25 @@ class MoviesController < ApplicationController
 			session[:mine] = mine
 			@movies = Movie.where(rating: [session[:mine]])
 			@all_ratings = Movie.all_ratings
-			flash.keep
 		when params[:sort_by] == "title" && session[:ratings] != nil
-			@movies = Movie.where(rating: [session[:mine]]).order("title")
+			@movies = Movie.where(rating: [session[:mine]]).order(:title)
 			@tit_class = 'hilite'
+			#this won't work here....hmmm  sort_by = session[:sort_by] = params[:sort_by]
 			@all_ratings = Movie.all_ratings
 			@checked = session[:ratings]
-			flash.keep
-			#redirect_to movies_path
 		when params[:sort_by] == "release_date" && session[:ratings] != nil
-			@movies = Movie.where(rating: [session[:mine]]).order("release_date")
-			@rel_class = 'hilite'
+			@movies = Movie.where(rating: [session[:mine]]).order(:release_date)
+			@rel_class = 'hilite'		
 			@all_ratings = Movie.all_ratings
 			@checked = session[:ratings]
-			flash.keep
-			#redirect_to movies_path
-		end	
+		end
+		
+		if params[:ratings] != session[:ratings] or session[:sort_by] != params[:sort_by]
+			session[:sort_by] = sort_by
+			session[:ratings] = @checked
+			redirect_to :sort_by => sort_by, :ratings => @checked and return
+		end
+		
   end
 
   def new
