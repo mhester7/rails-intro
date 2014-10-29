46145b9881e48556d3afcda1085c495574206554
Michael Hester <mhester7@uncc.edu>
2014-10-29 13:21:56 -0300

    hw 2 part 2 (maybe 3) complete_Different way - with case statements

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -24,17 +24,20 @@ class MoviesController < ApplicationController
 			session[:mine] = mine
 			@movies = Movie.where(rating: [session[:mine]])
 			@all_ratings = Movie.all_ratings
+			flash.keep
 		when params[:sort_by] == "title" && session[:ratings] != nil
 			@movies = Movie.where(rating: [session[:mine]]).order("title")
 			@tit_class = 'hilite'
 			@all_ratings = Movie.all_ratings
 			@checked = session[:ratings]
+			flash.keep
 			#redirect_to movies_path
 		when params[:sort_by] == "release_date" && session[:ratings] != nil
 			@movies = Movie.where(rating: [session[:mine]]).order("release_date")
 			@rel_class = 'hilite'
 			@all_ratings = Movie.all_ratings
 			@checked = session[:ratings]
+			flash.keep
 			#redirect_to movies_path
 		end	
   end
